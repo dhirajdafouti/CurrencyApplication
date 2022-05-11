38793562886f0e75b2ce9a1589945e79edc3e1aa
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-05-11 13:36:39 +0530

    The data and domain layer

--- app/src/main/java/com/project/currencyconvertorapplication/domain/usecases/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/project/currencyconvertorapplication/domain/usecases/get_coins/GetCoinsUseCase.kt
@@ -10,6 +10,7 @@ import retrofit2.HttpException
 import java.io.IOException
 import javax.inject.Inject
 
+
 class GetCoinsUseCase @Inject constructor(
     private val repository: CoinRepository,
 ) {
