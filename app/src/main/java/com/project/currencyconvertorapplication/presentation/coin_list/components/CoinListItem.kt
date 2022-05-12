2e3016ef4e6158a2cfe3177707ddc0f2a1bc50aa
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-05-12 22:18:40 +0530

    The coinlist UI

--- app/src/main/java/com/project/currencyconvertorapplication/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/project/currencyconvertorapplication/presentation/coin_list/components/CoinListItem.kt
@@ -5,13 +5,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.project.currencyconvertorapplication.domain.model.Coin
 
-@Preview
+
 @Composable
 fun CoinListItem(
     coin: Coin,
@@ -27,7 +33,18 @@ fun CoinListItem(
         horizontalArrangement = Arrangement.SpaceBetween
 
     ) {
-
+        Text(text = "${coin.rank}. ${coin.name}. ${coin.symbol}",
+            style = MaterialTheme.typography.body1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Text(text = if (coin.isActive) "active" else "inactive",
+            color = if (coin.isActive) Color.Green else Color.Red,
+            fontStyle = FontStyle.Italic,
+            modifier = Modifier.align(Alignment.CenterVertically),
+            textAlign = TextAlign.End
+        )
     }
-
 }
+
+
+
